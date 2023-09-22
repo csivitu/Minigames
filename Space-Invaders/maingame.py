76341bb20141366256786408306ee2ae9b1283b6
Tusker13-04 <63436014+Tusker13-04@users.noreply.github.com>
2023-09-22 21:30:40 +0530

    Update maingame.py
    Wrong path description

--- Space-Invaders/maingame.py
+++ Space-Invaders/maingame.py
@@ -11,23 +11,23 @@ WIDTH, HEIGHT = 800, 580
 WIN = pygame.display.set_mode((WIDTH, HEIGHT)) 
 pygame.display.set_caption("ISTE_Pygame")
 
-RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
-GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
-BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
-BOSS = pygame.image.load(os.path.join("assets", "goblin2.png"))
+RED_SPACE_SHIP = pygame.image.load(os.path.join("Space-Invaders", "pixel_ship_red_small.png"))
+GREEN_SPACE_SHIP = pygame.image.load(os.path.join("Space-Invaders", "pixel_ship_green_small.png"))
+BLUE_SPACE_SHIP = pygame.image.load(os.path.join("Space-Invaders", "pixel_ship_blue_small.png"))
+BOSS = pygame.image.load(os.path.join("Space-Invaders", "goblin2.png"))
 
 # ship ship
 
-YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "vibhor_ship.png"))
+YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("Space-Invaders", "vibhor_ship.png"))
 
 # lasers
-RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
-GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
-BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
-YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
+RED_LASER = pygame.image.load(os.path.join("Space-Invaders", "pixel_laser_red.png"))
+GREEN_LASER = pygame.image.load(os.path.join("Space-Invaders", "pixel_laser_green.png"))
+BLUE_LASER = pygame.image.load(os.path.join("Space-Invaders", "pixel_laser_blue.png"))
+YELLOW_LASER = pygame.image.load(os.path.join("Space-Invaders", "pixel_laser_yellow.png"))
 
 # background
-BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
+BG = pygame.transform.scale(pygame.image.load(os.path.join("Space-Invaders", "background-black.png")), (WIDTH, HEIGHT))
 
 #Laser
 class Laser:
